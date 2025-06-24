625b752c213a8f991f789758cfa788c4aff8f247
arian2004 <arian2004feni@gmail.com>
2025-06-24 12:49:22 +0600

    complete setup: tailwind, tanstack, daisyUI, motion, date-picker, router, react-tooltip, sweetalert

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center h-screen bg-base-100">
+        <h1 className="text-4xl font-bold text-blue-600">Hello, World!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
